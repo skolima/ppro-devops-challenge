9fb73ffd09b682f0c04554ccdb8dfc434bb98b7b
Leszek 'skolima' Ciesielski <skolima@gmail.com>
2022-09-20 15:01:53 +0200

    Basic EKS configuration

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
     region = var.region
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
 data "aws_availability_zones" "available" {}
 
 locals {
